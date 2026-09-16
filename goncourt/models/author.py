4aec7e7846389255e1d6303c57eb13a10ccc7eb1
Lucas Pillon <lucas.pillon@fms-ea.com>
2026-09-16 11:08:34 +0200

    model author

---
+++ goncourt/models/author.py
@@ -0,0 +1,14 @@
+from dataclasses import dataclass, field
+from typing import Optional
+
+
+@dataclass
+class Author:
+    """Classe d'un auteur"""
+    id: Optional[int] = field(default=None, init=False)
+    first_name: Optional[str] = field(default=None, init=False)
+    last_name: Optional[str] = field(default=None, init=False)
+    biography: Optional[str] = field(default=None, init=False)
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} {self.biography}"
