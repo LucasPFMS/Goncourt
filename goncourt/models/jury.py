44b992ebdd6ed9f854d91b1564c95c48bd28d2a5
Lucas Pillon <lucas.pillon@fms-ea.com>
2026-09-16 12:53:06 +0200

    model jury

---
+++ goncourt/models/jury.py
@@ -0,0 +1,13 @@
+from dataclasses import dataclass, field
+from typing import Optional
+from .author import Author
+
+
+@dataclass
+class Jury(Author):
+    """Classe d'un  membre du Jury"""
+    id: Optional[int] = field(default=None, repr=False)
+    is_president: Optional[bool] = field(default=None, repr=False)
+
+    def __str__(self) -> str:
+        return f"{self.last_name} {self.first_name} {self.is_president}"
