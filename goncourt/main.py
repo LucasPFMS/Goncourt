d6860a18ceb490763c1f75e072a08da63ee1016d
Lucas Pillon <lucas.pillon@fms-ea.com>
2026-09-18 08:51:18 +0200

    second selection main

--- goncourt/main.py
+++ goncourt/main.py
@@ -11,6 +11,8 @@ from models.book import Book
 
 def main() -> None:
     """Programme principal."""
+    second_selection = []
+    third_selection = []
     print("""\
 -------------------------------------------
 Bienvenue pour le résultat du prix Goncourt
@@ -30,7 +32,6 @@ Bienvenue pour le résultat du prix Goncourt
             print("voici la liste des livres, choisi l'id des 8 de la deuxième sélection")
             goncourt.display_books_list()
             i = 1
-            second_selection = []
             while i <= 8:
                 for book in goncourt.books:
                     id_chose = int(input())
@@ -38,7 +39,25 @@ Bienvenue pour le résultat du prix Goncourt
                         second_selection.append(goncourt.get_book_by_id(id_chose))
                         i += 1
 
+    print("Qui êtes vous ? (utilisateur/president")
+    is_president = input()
+    if is_president == "utilisateur":
+        # affichage de la liste des livres
+        goncourt.display_books_list()
+        print("deuxième sélection:")
+        print(second_selection)
 
+    else:
+        if is_president == "president":
+            print("voici la liste des livres, choisi l'id des 4 de la troisième sélection")
+            print(second_selection)
+            i = 1
+            while i <= 4:
+                for book in second_selection:
+                    id_chose = int(input())
+                    if book.id == id_chose:
+                        third_selection.append(goncourt.get_book_by_id(id_chose))
+                        i += 1
 
     # affichage de la liste des cours, leur enseignant et leurs élèves
 
